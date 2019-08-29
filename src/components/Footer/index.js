4e4ea23877ef91df955e98b55fc1bc541a0e6e2b
loannpham87 <loannpham87@gmail.com>
2019-08-28 20:36:13 -0700

    final working version

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">Clicky Game, IG edition!</div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Clicky Game - IG Edition!</h1>
+      <h2>
+        Click on an image to earn points, but your score resets if you click the
+        same one twice!
+      </h2>
+    </header>
+  );
+}
+
+export default Header;
